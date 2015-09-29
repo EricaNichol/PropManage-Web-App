2a7c054fb1782930da7185db7ec05eecd930d6ce
David Lin <davidlin98@hotmail.com>
2015-09-28 17:13:01 -0700

    Listing Show Page

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,10 @@
 class Listing < ActiveRecord::Base
   belongs_to :user
-  
+
   mount_uploader :image, ImageUploader
+
+
+  def full_address
+    "#{suite_no} #{address}, #{city}, #{postal}, #{country}"
+  end
 end

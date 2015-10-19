d6d57992f55b36c0011631993d0ccf08b5eeeb40
David Lin <davidlin98@hotmail.com>
2015-10-19 11:22:46 -0700

    sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        session[:user_id] = @user.id
         format.html { redirect_to feed_entries_path, notice: 'User was successfully created.' }
       else
         format.html { render :new }

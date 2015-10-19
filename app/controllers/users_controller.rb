bb1a044064b62204d9be0607f05d39131f8fdccd
David Lin <davidlin98@hotmail.com>
2015-10-19 11:19:04 -0700

    fixed signed in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to root_path, notice: 'User was successfully created.' }
+        format.html { redirect_to feed_entries_path, notice: 'User was successfully created.' }
       else
         format.html { render :new }
       end
